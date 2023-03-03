5beb3817c1323216855f7aa665f4fc04030920d7
blum <yuuybonito@gmail.com>
2023-03-03 15:53:44 -0300

    basic funtionality implemented

--- arraylist.c
+++ arraylist.c
@@ -1,1 +1,90 @@
+#include <stdlib.h>
+#include <string.h>
 #include "arraylist.h"
+
+struct arraylist_header {
+	size_t typ;
+	size_t siz;
+	size_t cap;
+};
+
+#define ARRAYLIST_HEADER(list) (((struct arraylist_header *)(list)) - 1)
+
+void *
+arraylist_alloc(size_t type, size_t size) {
+	struct arraylist_header *header = malloc(sizeof (struct arraylist_header) + type * (size + 1));
+	if (!header) return NULL;
+	header->typ = type;
+	header->siz = size;
+	header->cap = size+1;
+	return header + 1;
+}
+
+_Bool
+arraylist_push(void *restrict ptr_to_list, const void *restrict value) {
+	void **list = ptr_to_list;
+	struct arraylist_header *header = ARRAYLIST_HEADER(*list);
+	if (header->cap <= header->siz) {
+		header->cap *= 2;
+		header = realloc(header, sizeof (struct arraylist_header) + (header->typ * header->cap));
+		if (!header) {
+			*list = NULL;
+			return 0;
+		}
+	}
+	*list = header + 1;
+	memcpy((*list) + header->typ * header->siz++, value, header->typ);
+	return 1;
+}
+
+_Bool
+arraylist_pop(void *restrict list, void *restrict dest) {
+	struct arraylist_header *header = ARRAYLIST_HEADER(list);
+	if (!header->siz) return 0;
+	header->siz--;
+	if (!dest) return 1;
+	memcpy(dest, list + header->typ * header->siz, header->typ);
+	return 1;
+}
+
+_Bool
+arraylist_add(void *ptr_to_list, const void *value) {
+	void **list = ptr_to_list;
+	struct arraylist_header *header = ARRAYLIST_HEADER(*list);
+	if (header->cap <= header->siz) {
+		header->cap *= 2;
+		header = realloc(header, sizeof (struct arraylist_header) + (header->typ * header->cap));
+		if (!header) {
+			*list = NULL;
+			return 0;
+		}
+	}
+	*list = header + 1;
+	memmove((*list) + header->typ * header->siz++, value, header->typ);
+	return 1;
+}
+
+_Bool
+arraylist_delete(void *list, void *dest) {
+	struct arraylist_header *header = ARRAYLIST_HEADER(list);
+	if (!header->siz) return 0;
+	header->siz--;
+	if (!dest) return 1;
+	memmove(dest, list + header->typ * header->siz, header->typ);
+	return 1;
+}
+
+size_t
+arraylist_size(void *list) {
+	return ARRAYLIST_HEADER(list)->siz;
+}
+
+size_t
+arraylist_capacity(void *list) {
+	return ARRAYLIST_HEADER(list)->siz;
+}
+
+void 
+arraylist_free(void *list) {
+	free(ARRAYLIST_HEADER(list));
+}
--- arraylist.h
+++ arraylist.h
@@ -1,6 +1,68 @@
 #ifndef __ARRAYLIST_H__
 #define __ARRAYLIST_H__
 
+#include <stddef.h>
 
+/*
+ * arraylist_alloc:
+ * 	allocate list in the heap
+ * 	@param size_t type: size of members of the list
+ * 	@param size_t size: initial size of the list
+ * 	@returns the list if success or NULL if failure
+ * */
+void *arraylist_alloc(size_t type, size_t size);
+/*
+ * arraylist_alloc:
+ * 	push back the value into the list
+ * 	@param void *restrict ptr_to_list: a pointer to the list (I.e. &list)
+ * 	@param void *restrict value: a pointer to the value
+ * 	@returns 1 if success or 0 if failure
+ * */
+_Bool arraylist_push(void *restrict ptr_to_list, const void *restrict value);
+/*
+ * arraylist_alloc:
+ * 	pop back the list into the dest
+ * 	@param void *restrict list: the list
+ * 	@param void *restrict value: a pointer to the dest
+ * 	@returns 1 if success or 0 if failure
+ * */
+_Bool arraylist_pop(void *restrict list, void *restrict dest);
+/*
+ * arraylist_alloc:
+ * 	similar to arraylist_push, but allow memory collision between the list and the value
+ * 	@param void *ptr_to_list: a pointer to the list (I.e. &list)
+ * 	@param void *value: a pointer to the value
+ * 	@returns 1 if success or 0 if failure
+ * */
+_Bool arraylist_add(void *ptr_to_list, const void *value);
+/*
+ * arraylist_alloc:
+ * 	similar to arraylist_pop, but allow memory collision between the list and the value
+ * 	@param void *list: the list
+ * 	@param void *value: a pointer to the dest
+ * 	@returns 1 if success or 0 if failure
+ * */
+_Bool arraylist_delete(void *list, void *dest);
+/*
+ * arraylist_size:
+ * 	get the size of list
+ * 	@param void *list: the list
+ * 	@returns size of list
+ * */
+size_t arraylist_size(void *list);
+/*
+ * arraylist_capacity:
+ * 	get the capacity of list
+ * 	@param void *list: the list
+ * 	@returns capacity of list
+ * */
+size_t arraylist_capacity(void *list);
+/*
+ * arraylist_capacity:
+ * 	free just the list memory
+ * 	the members of list needs to be freed manually
+ * 	@param void *list: the list
+ * */
+void arraylist_free(void *list);
 
 #endif/*__ARRAYLIST_H__*/
